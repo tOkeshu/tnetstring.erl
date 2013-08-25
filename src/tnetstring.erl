c4b84d75ad731488344182cfc0d0d610a67c2c99
Romain Gauthier <romain.gauthier@monkeypatch.me>
2013-08-25 16:28:33 +0200

    Encode false

--- src/tnetstring.erl
+++ src/tnetstring.erl
@@ -5,5 +5,7 @@
 encode(null) ->
     <<"0:~">>;
 encode(true) ->
-    <<"4:true!">>.
+    <<"4:true!">>;
+encode(false) ->
+    <<"5:false!">>.
 
--- test/tnetstring_tests.erl
+++ test/tnetstring_tests.erl
@@ -9,3 +9,7 @@ encode_true_test() ->
     Payload = tnetstring:encode(true),
     ?assertEqual(<<"4:true!">>, Payload).
 
+encode_false_test() ->
+    Payload = tnetstring:encode(false),
+    ?assertEqual(<<"5:false!">>, Payload).
+
